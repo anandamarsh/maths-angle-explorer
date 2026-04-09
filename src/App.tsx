5e4e0c1df52fed05d43d63224173ad09864104d6
Amarsh Anand <amarsh.anand@example.com>
2026-04-09 14:35:31 +1000

    Bridge embedded commenter storage to parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import ArcadeAngleScreen from "./screens/ArcadeAngleScreen";
 import RotatePrompt from "./components/RotatePrompt";
 import { I18nProvider } from "./i18n";
+import { installEmbeddedStorageBridge } from "./utils/embeddedStorageBridge";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => installEmbeddedStorageBridge(), []);
+
   return (
     <I18nProvider>
       <RotatePrompt />
